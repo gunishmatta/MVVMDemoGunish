3d41cd5a85a81bed87c07850b72aac89571a9a79
gunishmatta <gunishmatta@gmail.com>
2022-01-22 15:04:19 +0530

    MVVM example done

--- app/src/main/java/com/gunish/mvvmdemogunish/MainActivity.java
+++ app/src/main/java/com/gunish/mvvmdemogunish/MainActivity.java
@@ -1,14 +1,31 @@
 package com.gunish.mvvmdemogunish;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.gunish.mvvmdemogunish.databinding.ActivityMainBinding;
+import com.gunish.mvvmdemogunish.viewmodels.LoginViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
     }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view,String message)
+    {
+        if(message!=null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
 }
